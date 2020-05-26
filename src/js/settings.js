7c254d642b2d3105f198257e78fd7513c57a4f26
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-05-26 19:22:02 +0200

    Add settings.js file

---
+++ src/js/settings.js
@@ -0,0 +1,14 @@
+const select = {
+  containerOf: {
+    topBar: '.top-bar',
+    sideBar: '.side-bar',
+  },
+  sideBar: {
+    logo: '.side-bar .logo',
+    menu: '.side-bar__menu',
+    messenger: '.side-bar__messenger',
+    bottomMenu: '.side-bar__menu--bottom',
+  },
+};
+
+export {select};

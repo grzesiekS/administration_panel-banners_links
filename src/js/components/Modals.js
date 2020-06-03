af37af4369b8e17293d473dac64d8a6a9c9ff152
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-03 20:27:50 +0200

    Add methods to add and remove class show for modals

--- src/js/components/Modals.js
+++ src/js/components/Modals.js
@@ -1,4 +1,4 @@
-import { select } from '../settings.js';
+import { select, classNames } from '../settings.js';
 
 class Modals {
   constructor(element){
@@ -12,28 +12,46 @@ class Modals {
   initActions() {
     const thisModal = this;
 
-    thisModal.closeModalBacground();
-
-    thisModal.topMenuLinks.forEach(element => {
-      element.addEventListener('click', function(){
-        event.preventDefault();
-
-      });
-    });
-  }
-
-  closeModalBackground() {
-    const thisModal = this;
+    /* Close modal by clicking background of overlay */
     document.querySelector('#' + select.containerOf.modals).addEventListener('click', function(e) {
       if(e.target === this) {
         thisModal.closeModal();
       }
     });
+
+    thisModal.topMenuLinks.forEach(element => {
+      element.addEventListener('click', function(){
+        event.preventDefault();
+        thisModal.openModal('#modal__' + element.getAttribute('href').replace('#',''));
+      });
+    });
   }
 
   closeModal() {
     const thisModal = this;
-    thisModal.dom.wrapper.classList.remove('show');
+    thisModal.dom.wrapper.classList.remove(classNames.modals.show);
+  }
+
+  openModal(modal) {
+    const thisModal = this;
+
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove(classNames.modals.show);
+    });
+
+    /*[DONE] Get selected modal */
+    const selectedModal = document.querySelector(modal);
+
+    /*START IF: If selected modal is not null */
+    if(selectedModal != null) {
+      /*[DONE] Add show class to selected modal */
+      selectedModal.classList.add(classNames.modals.show);
+
+      /*[DONE] Add show class for overlay class */
+      thisModal.dom.wrapper.classList.add(classNames.modals.show);
+
+    /*END IF: If selected modal is not null */
+    }
   }
 
   getElements(element) {

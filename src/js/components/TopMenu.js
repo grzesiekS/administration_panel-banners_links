1ba3175a35495b31c25d6c9497c56e23c61f2805
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-04 21:18:25 +0200

    Create initActions method in TopMenu

--- src/js/components/TopMenu.js
+++ src/js/components/TopMenu.js
@@ -5,13 +5,23 @@ class TopMenu {
     const thisTopMenu = this;
 
     thisTopMenu.getElements();
+    thisTopMenu.initActions();
+  }
+
+  initActions() {
+    const thisTopMenu = this;
+
+    thisTopMenu.links.forEach(element => {
+      element.addEventListener('click', function(){
+        console.log(element);
+      });
+    });
   }
 
   getElements() {
     const thisTopMenu = this;
 
     thisTopMenu.links = document.querySelectorAll(select.topMenuComponent.topMenuLinks);
-    console.log(thisTopMenu.links);
   }
 }
 

4208c40aa61ea534b009848faee82d1dccb9343b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-04 18:37:24 +0200

    Create methods validLogin and validPassword

--- src/js/components/ModalLogin.js
+++ src/js/components/ModalLogin.js
@@ -9,12 +9,36 @@ class ModalLogin extends Modals {
     thisModal.getElements();
   }
 
+  validLogin(){
+    const thisModal = this;
+
+    /* START IF: If length of login is greater then 0 */
+    if(thisModal.dom.inputLogin.value.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  validPassword(){
+    const thisModal = this;
+
+    /* START IF: If length of login is greater then 0 */
+    if(thisModal.dom.inputPassword.value.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   getElements() {
     const thisModal = this;
 
     thisModal.dom.login = thisModal.dom.wrapper.querySelector(select.modalSections.modalLogin);
     thisModal.dom.inputLogin = thisModal.dom.wrapper.querySelector(select.modalSections.modalLogin_login);
+    thisModal.dom.inputPassword = thisModal.dom.wrapper.querySelector(select.modalSections.modalLogin_password);
     console.log(thisModal.dom.inputLogin);
+    console.log(thisModal.dom.inputPassword);
   }
 }
 

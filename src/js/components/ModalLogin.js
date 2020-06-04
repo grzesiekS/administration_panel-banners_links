1fa004c4de47fc8a76c1e12f8b0495beae5d9e9f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-04 18:31:00 +0200

    Create getElements method in ModalLogin class

--- src/js/components/ModalLogin.js
+++ src/js/components/ModalLogin.js
@@ -1,8 +1,20 @@
 import { Modals } from './Modals.js';
+import {select} from '../settings.js';
 
 class ModalLogin extends Modals {
   constructor(element){
     super(element);
+
+    const thisModal = this;
+    thisModal.getElements();
+  }
+
+  getElements() {
+    const thisModal = this;
+
+    thisModal.dom.login = thisModal.dom.wrapper.querySelector(select.modalSections.modalLogin);
+    thisModal.dom.inputLogin = thisModal.dom.wrapper.querySelector(select.modalSections.modalLogin_login);
+    console.log(thisModal.dom.inputLogin);
   }
 }
 
